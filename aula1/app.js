81714adc9ce1b23876429cc0cd3f5a8fc97df6bb
SrBatatah <156966048+SrBatatah@users.noreply.github.com>
2025-01-22 18:18:43 -0300

    Update app.js

--- aula1/app.js
+++ aula1/app.js
@@ -1,15 +1,15 @@
 let numero = 0;
-let numeromaximo = parseInt(Math.random() * 200 + 1);
+let maximo = parseInt(Math.random() * 200 + 1);
 while (numero < 200) {
-    let numadc = parseInt(prompt(`Digite um novo número ate chegar em ${numeromaximo}: \no numero atual é ${numero}`));
+    let numadc = parseInt(prompt(`Digite um novo número ate chegar em ${maximo}: \no numero atual é ${numero}`));
     if (!isNaN(numadc)) {
         numero += numadc;
         alert(`O número atual é ${numero}`);
-        if (numero === numeromaximo) {
+        if (numero === maximo) {
             alert("Você acertou o número!");
             break;
         }
-        if (numero > numeromaximo) {
+        if (numero > maximo) {
             alert("Você ultrapassou o número!");
         }
     } else {
