911780ad29b96dd23d1949dead574afb424c9686
SrBatatah <156966048+SrBatatah@users.noreply.github.com>
2025-01-22 18:05:20 -0300

    Update app.js

--- aula1/app.js
+++ aula1/app.js
@@ -1,6 +1,6 @@
 let numero = parseInt(Math.random() * 200 + 1);
 while (numero < 10) {
-    let numadc = parseInt(prompt(`Digite um novo número ate chegar em 10: \no numero atual é ${numero}`));
+    let numadc = parseInt(prompt(`Digite um novo número ate chegar em ${numero}: \no numero atual é ${numero}`));
     if (!isNaN(numadc)) {
         numero += numadc;
         alert(`O número atual é ${numero}`);
